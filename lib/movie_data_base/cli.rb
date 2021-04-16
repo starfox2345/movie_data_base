5cc9831baa682fb58f68c5dc98527421c147eabd
yatsun <yatsunlam25@gmail.com>
2021-04-16 01:04:08 -0400

    Looking good so far...

--- lib/movie_data_base/cli.rb
+++ lib/movie_data_base/cli.rb
@@ -1,3 +1,20 @@
 class CLI
 
+    def start
+        puts "Welcome to my Movies Data Base!\nWhat is your name?"
+        input = user_input
+        greet(input)
+    end
+
+    def user_input
+        gets.strip
+    end
+
+    def greet(name)
+        puts "Greetings #{name}! Which Movie would you like to know about?"
+    end
+
+    def movie_list
+        
+    end
 end
--- lib/movie_data_base/movies.rb
+++ lib/movie_data_base/movies.rb
@@ -1,3 +1,20 @@
 class Movies
+    attr_accessor :movie_count, :limit, :page_number
 
+    @@all = []
+
+    def initialize(movie_count, limit, page_number)
+        @movie_count = movie_count
+        @limit = limit
+        @page_number = page_number
+        save
+    end
+
+    def save
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
 end
